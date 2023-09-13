ee8a5557d3806a05d7632ec81923f750c8a6abdc
Яшенков А.В <yashenkov@nkbvs.tsure.ru>
2023-09-13 10:23:36 +0300

    Update nvim

--- nvim/lua/fafaq/plugins/themes.lua
+++ nvim/lua/fafaq/plugins/themes.lua
@@ -1,7 +1,11 @@
 local M = {
-  'ellisonleao/gruvbox.nvim',
-  'lunarvim/darkplus.nvim',
-  'catppuccin/nvim'
+	"ellisonleao/gruvbox.nvim",
+	"lunarvim/darkplus.nvim",
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+	},
 }
 
 return M
